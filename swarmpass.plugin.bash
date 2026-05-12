a5874d18af11d4c0c5fd75a3b883a7af40688d4b
Fouad Atwi <fouad.atwi@canonical.com>
2026-05-13 00:38:10 +0200

    move bash completions to hive-core

--- swarmpass.plugin.bash
+++ swarmpass.plugin.bash
@@ -1,21 +1,7 @@
 . "${BASH_SOURCE%/*}/swarmpass"
 
-_swarmpass_instances()
-{
-    multipass list --format csv 2>/dev/null | awk -F, 'NR > 1 && $1 != "" { print $1 }'
-}
+_swarmpass_instances() { multipass list --format csv 2>/dev/null | awk -F, 'NR > 1 && $1 != "" { print $1 }'; }
 
-_swarmpass_completion()
-{
-    local cur instances
-
-    COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}"
-
-    [[ $COMP_CWORD -gt 1 ]] && return
-
-    instances="$(_swarmpass_instances | tr '\n' ' ')"
-    COMPREPLY=($(compgen -W "$instances" -- "$cur"))
-}
+_swarmpass_completion() { _fly_complete _swarmpass_instances; }
 
 complete -F _swarmpass_completion flympshell fmpshell flympbash fmpbash flympzsh fmpzsh flympksh fmpksh
